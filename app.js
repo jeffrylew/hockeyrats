0a2bc5ac6926593579f211f3945ae632b99eef1f
Jeffry <jeffryklew@gmail.com>
2020-03-16 22:19:03 -0400

    Implement contact route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 // const usersRouter = require('./routes/users');
 const aboutRouter = require('./routes/about');
 const lessonsRouter = require('./routes/lessons');
+const contactRouter = require('./routes/contact');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/lessons', lessonsRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
