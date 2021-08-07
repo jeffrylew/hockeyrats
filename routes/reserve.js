130de46fa026d5a672ceb4e8b1e8321d017a5562
Jeffry <jeffryklew@gmail.com>
2021-08-07 12:32:33 -0400

    Implement routes/reserve.js with confirmation email system

--- routes/reserve.js
+++ routes/reserve.js
@@ -4,29 +4,104 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-/* Send reservation confirmation emails */
-router.post('/', (req, res) => {
-  const msg = {
-    to: 'jeffryklew@gmail.com',
+/**
+ * Define Dynamic Transactional Template data for email to customer
+ * @param {Object} body - Body of request to post route
+ * @param {number} order_id - ID of reservation request
+ */
+const confirmationEmail = (body, order_id) => {
+  const { items, cartName, cartEmail, cartPhone, cartMsg } = body;
+
+  return {
+    to: [
+      {
+        name: cartName,
+        email: cartEmail
+      },
+    ],
     from: 'support@hockeyrats.com',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    templateId: process.env.SENDGRID_RESERVATION_TEMPLATE_ID,
+    dynamicTemplateData: {
+      order_id: order_id,
+      items: items,
+      cartName: cartName,
+      cartEmail: cartEmail,
+      cartPhone: cartPhone,
+      cartMsg: cartMsg,
+      receipt: true
+    }
   };
 
+} // const confirmationEmail = ...
+
+/**
+ * Define Dynamic Transactional Template data for email to Coach Joe
+ * @param {Object} body - Body of request to post route
+ * @param {number} order_id - ID of reservation request
+ */
+const requestEmailJoe = (body, order_id) => {
+  const { items, cartName, cartEmail, cartPhone, cartMsg } = body;
+
+  return {
+    to: [
+      {
+        name: 'Joe Wagner',
+        email: 'josephwagner74@yahoo.com'
+      },
+    ],
+    from: 'support@hockeyrats.com',
+    templateId: process.env.SENDGRID_RESERVATION_JOE_TEMPLATE_ID,
+    dynamicTemplateData: {
+      order_id: order_id,
+      items: items,
+      cartName: cartName,
+      cartEmail: cartEmail,
+      cartPhone: cartPhone,
+      cartMsg: cartMsg,
+      receipt: true
+    }
+  };
+
+} // const requestEmailJoe = ...
+
+
+/* Prevent GET reserve route from being accessed directly */
+router.get('/', (req, res) => {
+  res.redirect(403, process.env.DOMAIN);
+});
+
+
+/* Send reservation request emails */
+router.post('/', (req, res) => {
+  // Get unixtime through date-fns
+  const getUnixTime = require('date-fns/getUnixTime');
+  const unixtime    = getUnixTime(new Date());
+
+  // Send confirmation emails to customer and Coach Joe
   sgMail
-    .send(msg)
-    .then(() => {}, error => {
-      console.error(error);
-
-      if (error.response) {
-        console.error(error.response.body);
-      }
+    .send(confirmationEmail(req.body, unixtime))
+    .then(() => sgMail.send(requestEmailJoe(req.body, unixtime)))
+    .then(() => res.status(200).send({
+      successURL: process.env.DOMAIN + '/reserve/success?order_id=' + unixtime
+    }))
+    .catch(error => {
+      res.sendStatus(500);
     });
+});
 
-  res.render('success', {
-    session_id: "helloworld"
-  });
+
+/* GET success page */
+router.get('/success', (req, res) => {
+  // Get order_id query value
+  const { order_id } = req.query;
+
+  if (order_id === undefined || order_id === "") {
+    res.redirect(404, process.env.DOMAIN);
+  } else {
+    res.render('reservation_success', {
+      order_id: order_id
+    });
+  }
 });
 
 module.exports = router;
